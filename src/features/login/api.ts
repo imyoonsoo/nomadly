95cf3ac750eec157819f9008c070e6239ff33805
imyoonsoo <bliss38soo@gmail.com>
2026-09-08 20:37:26 +0900

    fix: 로그인 엔드포인트 수정

--- src/features/login/api.ts
+++ src/features/login/api.ts
@@ -4,7 +4,7 @@ import type { LoginParams, LoginResponse } from "./type";
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth`;
 
 const login = async (body: LoginParams): Promise<LoginResponse> => {
-  const response = await axios.post(`${BASE_URL}/login`, body);
+  const response = await axios.post(`${BASE_URL}/tokens`, body);
   return response.data;
 };
 
